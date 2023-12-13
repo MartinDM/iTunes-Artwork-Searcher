0c62001bf99d742e95dfd7fa13d20dc20a2d608d
martindm <hello@martindm.uk>
2023-12-13 04:53:43 +0000

    WIP component=split

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import { useEffect, useState } from 'react';
-import {
-  Box,
-  Image,
-  Text,
-  Input,
-  Link,
-  Select,
-  Heading,
-  Center,
-} from '@chakra-ui/react';
+import { Box, Text, Input, Select, Heading, Center } from '@chakra-ui/react';
 import './App.css';
 import { Button } from '@chakra-ui/react';
 import { Container } from '@chakra-ui/react';
 import { SimpleGrid } from '@chakra-ui/react';
-import { IoCloudDownloadOutline } from 'react-icons/io5';
 import { AiOutlineLoading } from 'react-icons/ai';
+import Result from './components/result';
 
 enum entityTypes {
   Album = 'album',
@@ -61,11 +52,6 @@ function App() {
     }
   };
 
-  const getThumb = (url: string, size: string): string => {
-    if (!url) return '';
-    return url.replace('100x100bb', `${size}x${size}bb`);
-  };
-
   const getArtwork = () => {
     setIsSearching(true);
     fetch(
@@ -82,19 +68,6 @@ function App() {
         setIsSearching(false);
         console.log(error);
       });
-  };
-
-  const linkProps = {
-    _hover: {
-      cursor: 'pointer',
-      textDecoration: 'underline',
-      color: '#ff9393',
-    },
-    display: 'flex',
-    fontWeight: '500',
-    alignItems: 'center',
-    gap: '2',
-    my: 2,
   };
 
   return (
@@ -153,65 +126,7 @@ function App() {
           {results &&
             results.map((result) => {
               const hasImage = result.artworkUrl100;
-              return (
-                <Box
-                  key={result.collectionId}
-                  maxW="sm"
-                  borderWidth="1px"
-                  borderColor={'#e4e4e4'}
-                  borderRadius="lg"
-                  bg={'gray.800'}
-                >
-                  {hasImage && (
-                    <Image
-                      src={getThumb(result.artworkUrl100, '270')}
-                      alt={result.collectionName}
-                      width="100%"
-                    />
-                  )}
-                  <Box color="#FF5E5E" p="4" pb={5}>
-                    <Box mb={4}>
-                      <Text fontWeight="semibold" fontSize="xl">
-                        {result.artistName}
-                      </Text>
-                      <Text
-                        fontSize="sm"
-                        fontWeight="normal"
-                        letterSpacing="wide"
-                      >
-                        {result?.collectionName}
-                      </Text>
-                    </Box>
-                    {hasImage ? (
-                      <Box
-                        fontWeight="500"
-                        alignItems={'center'}
-                        gap={2}
-                        my={2}
-                      >
-                        <Link
-                          {...linkProps}
-                          href={getThumb(result.artworkUrl100, '600')}
-                          isExternal
-                        >
-                          <IoCloudDownloadOutline />
-                          Standard res (600px)
-                        </Link>
-                        <Link
-                          {...linkProps}
-                          href={getThumb(result.artworkUrl100, '2000')}
-                          isExternal
-                        >
-                          <IoCloudDownloadOutline />
-                          Highest res (2000px)
-                        </Link>
-                      </Box>
-                    ) : (
-                      `No artwork for ${result.collectionName}`
-                    )}
-                  </Box>
-                </Box>
-              );
+              return <Result result={result} />;
             })}
         </SimpleGrid>
         {isSearching && (
---
+++ src/components/Result.tsx
@@ -0,0 +1,84 @@
+import {
+  Box,
+  Image,
+  Text, 
+  Link, 
+} from '@chakra-ui/react';
+
+import { IoCloudDownloadOutline } from 'react-icons/io5';
+const linkProps = {
+  _hover: {
+    cursor: 'pointer',
+    textDecoration: 'underline',
+    color: '#ff9393',
+  },
+  display: 'flex',
+  fontWeight: '500',
+  alignItems: 'center',
+  gap: '2',
+  my: 2,
+};
+ 
+
+const getThumb = (url: string, size: string): string => {
+    if (!url) return '';
+    return url.replace('100x100bb', `${size}x${size}bb`);
+  };
+
+const Result = ({result}) => {
+
+return (
+
+
+  <Box
+    key={result.collectionId}
+    maxW="sm"
+    borderWidth="1px"
+    borderColor={'#e4e4e4'}
+    borderRadius="lg"
+    bg={'gray.800'}
+  >
+    {hasImage && (
+      <Image
+        src={getThumb(result.artworkUrl100, '270')}
+        alt={result.collectionName}
+        width="100%"
+      />
+    )}
+    <Box color="#FF5E5E" p="4" pb={5}>
+      <Box mb={4}>
+        <Text fontWeight="semibold" fontSize="xl">
+          {result.artistName}
+        </Text>
+        <Text fontSize="sm" fontWeight="normal" letterSpacing="wide">
+          {result?.collectionName}
+        </Text>
+      </Box>
+      {hasImage ? (
+        <Box fontWeight="500" alignItems={'center'} gap={2} my={2}>
+          <Link
+            {...linkProps}
+            href={getThumb(result.artworkUrl100, '600')}
+            isExternal
+          >
+            <IoCloudDownloadOutline />
+            Standard res (600px)
+          </Link>
+          <Link
+            {...linkProps}
+            href={getThumb(result.artworkUrl100, '2000')}
+            isExternal
+          >
+            <IoCloudDownloadOutline />
+            Highest res (2000px)
+          </Link>
+        </Box>
+      ) : (
+        `No artwork for ${result.collectionName}`
+      )}
+    </Box>
+  </Box>;
+)
+};
+
+export default Result;
